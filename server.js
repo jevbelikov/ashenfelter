502e81ad92b30a7a2f0c6ac7395b5357fba0b1c5
EB <eb120@hw.ac.uk>
2016-01-31 19:36:43 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+/* 
+* name: server.js
+* description: node app 
+*/
+var express = require('express');
+var serveStatic = require('serve-static');
+
+// environment var port is being set by heroku
+var port = process.env.port || 8080;  
+ 
+var app = express();
+app.use(serveStatic(__dirname + '/public'));
+app.listen(port);
+
